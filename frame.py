a6b3d7fa2306471228c9203df9c78f8331031b23
dtreich-itdp <taylor.reich@itdp.com>
2019-11-15 15:39:10 -0500

    fixed calls to subprocess

--- frame.py
+++ frame.py
@@ -23,16 +23,16 @@ def from_id_hdc(hdc):
         out.write(test_city)
     #take extract from planet.pbf
     command = "osmium extract -p {}/boundaries.geojson -d {}/ -s simple -v -o city.pbf".format(str(hdc), str(hdc))
-    subprocess.call(command)
+    subprocess.call(command.split(' '))
     command = "osmconvert {}/city.pbf >{}/city.pbf".format(str(hdc),str(hdc))
-    subprocess.call(command)
+    subprocess.call(command.split(' '))
     command = '''osmfilter {}/city.o5m 
     --drop="area=yes highway=link =motor =proposed 
     =construction =abandoned =platform =raceway 
     service=parking_aisle =driveway =private foot=no" 
     --keep="highway" >{}/city.osm
     '''.format(str(hdc),str(hdc))
-    subprocess.call(command)
+    subprocess.call(command.split(' '))
     
     
     #results = people_near_services.pnservices(city, folder_name=str(hdc)+'/')
