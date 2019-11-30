fd03ace83d16e7390efe3e83df63d12f6793d538
dtreich-itdp <taylor.reich@itdp.com>
2019-11-30 12:57:05 -0500

    added pdb call

--- local_isometric.py
+++ local_isometric.py
@@ -162,7 +162,8 @@ def plot(G, isochrone_polys, center_nodes, distances):
 
 def export(polygons, epsg, service, folder=""):
     #returns the result both latlon, projected, but only saves it latlon in geojson
-
+    import pdb
+    pdb.set_trace()
     a = pd.DataFrame(polygons).T
     a = pd.melt(a, value_vars=a.columns)
     a.columns = ['distance', 'polygon']
