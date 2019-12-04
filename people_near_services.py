1ee61e8092e42ddab505f85f38834588bf5bf4b5
dtreich-itdp <taylor.reich@itdp.com>
2019-12-04 14:36:21 -0500

    Update people_near_services.py

--- people_near_services.py
+++ people_near_services.py
@@ -94,11 +94,6 @@ def pnservices(city, folder_name='', buffer_dist=100, headway_threshold=10,
     n_vslicers = math.floor(height_km / patch_length)
     n_vslicers = math.floor(width_km / patch_length)
     
-    n_hslicers = math.floor(round(total_pop/200000)/2) #change to ????
-    hslicers=[]
-    n_vslicers = math.ceil(round(total_pop/200000)/2) #change to ????
-    vslicers=[]
-    
     for i in range(1,n_hslicers+1):
         increment = (bbox[2]-bbox[0])/(n_hslicers+1)
         lat = bbox[0]+(i*increment)
