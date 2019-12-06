36da74295debf26684cc279bdd36adbc105e4470
taylor reich <taylor.reich@itdp.org>
2019-12-06 17:59:50 -0500

    hi

---
+++ frame.py
@@ -0,0 +1,40 @@
+import subprocess
+import fiona
+import os
+import json
+
+import people_near_services
+
+
+def from_id_hdc(hdc, to_test = None):
+    #select city from ID number
+    with fiona.open('GHS_STAT_UCDB2015MT_GLOBE_R2019A_V1_0.shp','r') as ucdb:
+        for city in ucdb:
+            if city['properties']['ID_HDC_G0'] == hdc:
+                test_city = city
+    #save city geometry so that I can take an extract from planet.pbf within it
+    if not os.path.isdir(str(hdc)):
+        os.mkdir(str(hdc))
+    with open(str(hdc)+'/boundaries.geojson', 'w') as out:
+        out.write(json.dumps(test_city))
+    #take extract from planet.pbf
+    command = "osmium extract planet-latest.osm.pbf -p {}/boundaries.geojson -s complete_ways -v -o {}/city.pbf".format(str(hdc), str(hdc))
+    print(command)
+    subprocess.check_call(command.split(' '))
+    command = "osmconvert {}/city.pbf -o={}/city.o5m".format(str(hdc),str(hdc))
+    print(command)
+    subprocess.check_call(command.split(' '))
+    command = ['osmfilter', '{}/city.o5m'.format(str(hdc)),
+    '--drop="area=yes highway=link =motor =proposed =construction =abandoned =platform =raceway service=parking_aisle =driveway =private foot=no"', '--keep="highway"', '-o={}/citywalk.o5m'.format(str(hdc))]
+    print(command)
+    subprocess.check_call(command)
+    
+    if to_test:
+        results = people_near_services.pnservices(test_city, to_test = to_test, folder_name=str(hdc)+'/')
+    else:
+        results = people_near_services.pnservices(test_city, folder_name=str(hdc)+'/')
+
+    print(str(results))
+    
+from_id_hdc(1367)
+
