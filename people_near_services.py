f194052272ffdd6900620486653cd8225efc3271
dtreich-itdp <taylor.reich@itdp.com>
2020-01-21 19:29:04 -0500

    Update people_near_services.py

--- people_near_services.py
+++ people_near_services.py
@@ -308,7 +308,7 @@ def pnservices(city, folder_name='', buffer_dist=100, headway_threshold=10,
                     print(crs)
                     projection = pyproj.Transformer.from_crs(4326, crs)
                     carfree = shapely.ops.transform(projection.transform, carfree)
-                    isochrone_polys['carfree'] = local_isometric.network_buffer(carfree, distance=buffer_dist)
+                    isochrone_polys['carfree'] = carfree.buffer(buffer_dist)
                 
             
             
