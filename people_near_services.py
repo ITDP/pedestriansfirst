8fbdd46060dcc3d4d581c1b79241ff606f5b17ed
dtreich-itdp <taylor.reich@itdp.com>
2020-01-30 11:46:51 -0500

    dfg

--- people_near_services.py
+++ people_near_services.py
@@ -550,6 +550,7 @@ def pnservices(city, folder_name='', buffer_dist=100, headway_threshold=10,
     
     ft = datetime.datetime.now()
     print("total", str(ft-dt))
+    results['calctime':ft-dt]
     with open(folder_name+"results.json","w") as output:
         output.write(json.dumps(results))
     return results
