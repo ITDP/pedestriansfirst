a9a1c97f51b2106733edb5f9fef1598490614819
dtreich-itdp <taylor.reich@itdp.com>
2020-01-30 17:45:07 -0500

    Update frame.py

--- frame.py
+++ frame.py
@@ -86,9 +86,9 @@ hdcs = { #test
 'Ahmadabad': 6651
         }
 
-from_id_hdc(14)
-shutil.copytree('14/','14tenmin/')
-from_id_hdc(14,kwargs = {'headway_threshold': 20, 'to_test': ['transit']})
+from_id_hdc(14,kwargs = {'headway_threshold': 20})
+shutil.copytree('14/','14twentymin/')
+from_id_hdc(14, kwargs = {'to_test': ['transit']})
 
 
 for city in hdcs.keys():
