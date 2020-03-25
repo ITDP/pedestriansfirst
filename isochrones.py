31d523f7a3d607d7ac10b5626951e288b7624aad
dtreich-itdp <taylor.reich@itdp.com>
2020-03-25 10:48:32 -0400

    Update isochrones.py

--- isochrones.py
+++ isochrones.py
@@ -79,7 +79,7 @@ def make_iso_polys(G, center_nodes, distance=500,
                 # try to fill in surrounded areas so shapes will appear solid
                 new_iso = Polygon(new_iso.exterior)
             polygons.append(new_iso)
-        except KeyError:
+        except KeyError as erro:
             print ("FAILURE AT",i,center_node)
             #It's normal for a small fraction of center_nodes to not appear
             #when this is used for Pedestrians First.
