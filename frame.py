0aa0fe50c273bda2d56dd1dbac7c87b4ed4e5f18
D. Taylor Reich <taylor.reich@itdp.org>
2023-08-22 21:15:59 -0400

    added commandline arguments to control unique selections

--- frame.py
+++ frame.py
@@ -606,9 +606,6 @@ if __name__ == '__main__':
 11223	,
 14	,
         ]
-    
-    for cityid in test_cities:
-        regional_analysis(cityid)
 
     ucdb = gpd.read_file('input_data/old_ghsl/GHS_STAT_UCDB2015MT_GLOBE_R2019A_V1_2.gpkg')
     for hdc in ucdb[(500000 < ucdb.P15)&(ucdb.P15 < 10000000000)].sort_values('P15', ascending=False).ID_HDC_G0:
