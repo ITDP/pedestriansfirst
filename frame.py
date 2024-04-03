db2b41dd1cb8888c92e1a04a4c8931904d2b7d69
D. Taylor Reich <taylor.reich@itdp.org>
2024-04-03 15:53:37 -0400

    worked on country indicators

--- frame.py
+++ frame.py
@@ -516,6 +516,7 @@ def calculate_country_indicators(current_year=2022,
                     for year in rt_and_pop_years:
                         total_pop_year = city_results[city_results.country == country][f'total_pop_{year}'].sum()
                         country_totals.loc[country, f'total_pop_{year}'] += total_pop_year
+                        import pdb; pdb.set_trace()
                         if city_results[city_results.country == country]['has_gtfs'].iloc[0] == 'True':
                             country_totals.loc[country, f'total_pop_gtfs_cities_only_{year}'] += total_pop_year
                     #then indicators based on sums
