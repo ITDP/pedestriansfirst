12be7d1a1545cbb218118fa9c238ab68e2a63e74
D. Taylor Reich <taylor.reich@itdp.org>
2024-06-21 11:41:36 -0400

    generalized ghsl download

--- frame.py
+++ frame.py
@@ -77,18 +77,19 @@ def prep_from_poly(poly, folder_name, boundary_buffer = 500):
     
     
 def download_ghsl(proj='mw', resolution='100'):
+    proj_code = {'mw':54009,'ll':4326}[proj]
     if not os.path.exists('input_data/'):
         os.mkdir('input_data/')
-    if not os.path.exists('input_data/ghsl_data_100m_mw/'):
-        os.mkdir('input_data/ghsl_data_100m_mw/')
+    if not os.path.exists('input_data/ghsl_data_{resolution}m_{proj}/'):
+        os.mkdir('input_data/ghsl_data_{resolution}m_mw/')
     for year in tqdm(range(1975, 2031, 5)): 
         letter='E'
         if proj == 'mw':
-            name = f'GHS_POP_{letter}{year}_GLOBE_R2023A_54009_{resolution}'
-        if not os.path.exists(f'input_data/ghsl_data_100m_mw/{name}/{name}_V1_0.tif'):
+            name = f'GHS_POP_{letter}{year}_GLOBE_R2023A_{proj_code}_{resolution}'
+        if not os.path.exists(f'input_data/ghsl_data_{resolution}m_{proj}/{name}/{name}_V1_0.tif'):
             zippath, _ = urllib.request.urlretrieve(f'https://jeodpp.jrc.ec.europa.eu/ftp/jrc-opendata/GHSL/GHS_POP_GLOBE_R2023A/{name}/V1-0/{name}_V1_0.zip')
             with zipfile.ZipFile(zippath, "r") as f:
-                f.extractall(f'input_data/ghsl_data_100m_mw/{name}')
+                f.extractall(f'input_data/ghsl_data_{resolution}m_{proj}/{name}')
     
     
 # def from_id_hdc(hdc, folder_prefix = 'cities_out', boundary_buffer = 500, kwargs = {}):
