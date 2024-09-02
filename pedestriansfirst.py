b078334bbf3bdaf7c8264bc4673e1e616e8636f8
D. Taylor Reich <taylor.reich@itdp.org>
2024-09-02 16:09:21 +0300

    fixed cities with no RT (i hope)

--- pedestriansfirst.py
+++ pedestriansfirst.py
@@ -355,8 +355,9 @@ def spatial_analysis(boundaries,
             rt_lines.loc[lineidx,'rt_mode'] = itdp_mode
         
         rt_lines = rt_lines[rt_lines.rt_mode.isna() == False]
-        rt_lines_utm = ox.project_gdf(rt_lines)
-        rt_stns_utm = ox.project_gdf(rt_stns)
+        if len(x) > 0:
+            rt_lines_utm = ox.project_gdf(rt_lines)
+            rt_stns_utm = ox.project_gdf(rt_stns)
         
         for lineidx in rt_lines.index:
             selected_stns = rt_stns[(rt_stns_utm.intersects(rt_lines_utm.loc[lineidx,'geometry'].buffer(200))) & (rt_stns['mode'] == rt_lines.loc[lineidx, 'mode'])]
